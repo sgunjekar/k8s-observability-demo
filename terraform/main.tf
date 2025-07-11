91d0e82eeb37ba796ee57ff10998a5e0a22f3162
sushil gunjekar <sushil.gunjekar@gmail.com>
2025-07-12 04:36:49 +0530

    fix:terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,76 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_security_group" "k8s_sg" {
+  name        = "k8s_sg"
+  description = "Allow SSH, HTTP, NodePort range"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "k8s_node" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.medium"
+  ami                    = "ami-0c02fb55956c7d316" # Ubuntu 22.04 (check latest)
+  instance_type          = "t2.medium"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.k8s_sg.id]
+  associate_public_ip_address = true
+  key_name               = "demo-ec2-keypair"
+
+  tags = {
+    Name = "k8s-observability-node"
+  }
 }
