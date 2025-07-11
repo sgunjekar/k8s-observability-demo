19e2d5732c116c2677b2d2a11b8763dbc86e03f0
sushil gunjekar <sushil.gunjekar@gmail.com>
2025-07-12 04:41:48 +0530

    fix:scripts:argocd

--- scripts/argocd-register-repo.sh
+++ scripts/argocd-register-repo.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
 argocd login localhost:8080 --username admin --password admin
-argocd repo add https://github.com/your-org/k8s-observability-demo.git
-argocd app create bootstrap --repo https://github.com/your-org/k8s-observability-demo.git --path argocd --dest-server https://kubernetes.default.svc --dest-namespace argocd
+
+argocd repo add https://github.com/sgunjekar/k8s-observability-demo.git
+
+argocd app create bootstrap \
+  --repo https://github.com/sgunjekar/k8s-observability-demo.git \
+  --path argocd/bootstrap \
+  --dest-server https://kubernetes.default.svc \
+  --dest-namespace argocd \
+  --sync-policy automated
